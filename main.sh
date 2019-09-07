29652e8540014ace7161339369ac5be88768ab5d
Vale <valyatev@gmail.com>
2019-09-07 13:46:09 -0500

    case in main

--- main.sh
+++ main.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 apps=()
-for arg in "$@"
-do
-	input="./apps/$arg"
-	while IFS= read -r line
-	do
-	  echo "$line"
-	  apps+=" $line "
-	done < "$input"
-done
 
-exec ./installer.sh "${apps[@]}"
+echo $1
+
+
+case $1 in
+
+  show)
+    printf "Lista de paquetes disponibles para instalación: \n"
+	ls ./apps/
+	printf "Lista de paquetes disponibles para configuración: \n"
+	ls ./scripts_config/
+    ;;
+
+  install)
+    for arg in "$@"
+		do
+			input="./apps/$arg"
+			while IFS= read -r line
+			do
+			  echo "$line"
+			  apps+=" $line "
+			done < "$input"
+		done
+		exec ./installer.sh "${apps[@]}"
+    ;;
+
+  config)
+    echo -n "Italian"
+    ;;
+
+  *)
+    echo -n "help"
+    ;;
+esac
